0498eb5a22b1812d5b5b27a1021a49fc8ebcb03e
beanieo-o <yexing_work@163.com>
2023-12-07 13:00:07 +0800

    first

---
+++ ActivityDiary-master/app/src/androidTest/java/de/rampro/activitydiary/MainActivityInstrumentedTest.java
@@ -0,0 +1,39 @@
+package de.rampro.activitydiary;
+
+import android.content.Context;
+import android.support.*;
+import androidx.test.*;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentedTest {
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("de.rampro.activitydiary.debug", appContext.getPackageName());
+    }
+
+    @Test
+    public void createMainActivity() throws Exception {
+/*        final Menu menu = shadowOf(activity).getOptionsMenu();
+        assertNotNull(activity);
+
+        assertTrue(menu.findItem(R.id.action_add_activity).isEnabled());*/
+
+    }
+}
---
+++ ActivityDiary-master/app/src/main/java/de/rampro/activitydiary/ui/generic/AddPlan.java
@@ -0,0 +1,42 @@
+/*
+ * ActivityDiary
+ *
+ * Copyright (C) 2023 Raphael Mack http://www.raphael-mack.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.rampro.activitydiary.ui.generic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import de.rampro.activitydiary.R;
+import android.view.View;
+
+public class AddPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_plan);
+    }
+
+    public void JumpToActivityContent(View view){
+        Intent intent = new Intent(this,EditActivity.class);
+        startActivity(intent);
+    }
+}
